07d0fa59ef93e425d190ae013a7f6f65bc8fe8c6
timtimer11 <justanestudio@gmail.com>
2025-06-19 14:20:09 +0700

    Update reddit fetch header

--- api/clients/reddit.py
+++ api/clients/reddit.py
@@ -6,7 +6,20 @@ class RedditClient:
     This class is used to fetch a post and its comments from a given URL and return the post data.
     """
     def __init__(self):
-        self.headers = {'User-Agent': 'Mozilla/5.0'}
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Cache-Control': 'max-age=0'
+        }
 
     def fetch_post(self, url: str) -> Dict:
         """

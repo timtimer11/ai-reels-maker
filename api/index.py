bb32ebbf36fe5ab8393adf52fac80d179922eea3
timtimer11 <justanestudio@gmail.com>
2025-06-19 11:54:44 +0700

    Update CORS for Vercel URL

--- api/index.py
+++ api/index.py
@@ -7,7 +7,10 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Allows requests from Next.js dev server
+    allow_origins=[
+        "http://localhost:3000",  # Local development
+        "https://ai-reels-maker.vercel.app/generate-video" 
+    ],
     allow_credentials=True, # Allows cookies and authentication
     allow_methods=["*"], # Allows all HTTP methods (GET, POST, etc.)
     allow_headers=["*"], # Allows all HTTP headers

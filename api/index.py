39eeeb4e65a23f5ca2a956aa4c903268d9844bac
timtimer11 <justanestudio@gmail.com>
2025-06-27 01:11:02 +0700

    Add sharp

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # Local development
-        "https://ai-reels-maker.vercel.app"  # Your Vercel deployment
+        "https://ai-reels-maker.vercel.app",  # Your Vercel deployment
         "https://ai-reels-maker-production.up.railway.app/" # Railway deployment
     ],
     allow_credentials=True, # Allows cookies and authentication
